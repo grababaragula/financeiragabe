d77de6f8c64be35d16a155bc75cd757f059bfeaa
unknown <you@example.com>
2022-08-18 19:58:53 -0300

    teste

--- financeira/settings.py
+++ financeira/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@j%tq#8rwslyp_2q1!-ly1qibhps+t18^0b@p)a6p4)0v)rv$_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
