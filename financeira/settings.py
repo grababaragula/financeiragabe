ea58dfcbb88abdee41424986064491250c14f616
unknown <you@example.com>
2022-08-18 19:52:19 -0300

    teste

--- financeira/settings.py
+++ financeira/settings.py
@@ -124,7 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'templates/static')
-]
-django_heroku.settings(locals())
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
